347a20d2cb3250f94176c48125c051d7e44233fa
Isidora Brasancevic <isidorabr@gmail.com>
2024-03-07 15:12:37 +0100

    Added user service tests

--- src/test/java/com/example/movieClub/MovieTestData.java
+++ src/test/java/com/example/movieClub/MovieTestData.java
@@ -2,6 +2,7 @@ package com.example.movieClub;
 
 import com.example.movieClub.model.Movie;
 import com.example.movieClub.model.MovieCopy;
+import com.example.movieClub.model.User;
 
 public class MovieTestData {
 
@@ -11,5 +12,9 @@ public class MovieTestData {
 
     public static MovieCopy movieCopyBuilder(int copyNumber, Movie movie) {
         return MovieCopy.builder().copyNumber(copyNumber).movie(movie).build();
+    }
+
+    public static User userBuilder(String name, String email) {
+        return User.builder().name(name).email(email).build();
     }
 }
