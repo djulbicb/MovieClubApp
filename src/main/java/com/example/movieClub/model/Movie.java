725d0eae9da09315896a013f1ef4ddb3471b6266
Isidora Brasancevic <isidorabr@gmail.com>
2024-03-16 16:54:39 +0100

    Added new movie and movie dto fealds

--- src/main/java/com/example/movieClub/model/Movie.java
+++ src/main/java/com/example/movieClub/model/Movie.java
@@ -20,6 +20,9 @@ public class Movie {
     private Long id;
     private String name;
     private String genre;
+    private String description;
+    private String imageUrl;
+    private int year;
     @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<MovieCopy> movieCopies;
 }
